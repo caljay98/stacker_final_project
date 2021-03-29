364ae8e8cf577ea270dc5441f0121c5fbff8f991
caljay98 <caljay98@gmail.com>
2021-03-28 23:00:57 -0500

    added some ASM into refresh_display(), very untested and scary

--- neopixel_display_lib.c
+++ neopixel_display_lib.c
@@ -10,9 +10,9 @@
 // global variables
 // the frame buffer read from. Must be 64 pixels long, with 3 bytes for pixels.
 // Stored in the same order as the neopixel, G-R-B.
-volatile uint8_t frame_buffer[NUM_OF_PIX*BYTES_PER_PIX];
-volatile uint8_t current_byte = 0;              // counter for each led
-volatile uint8_t current_bit = 0;               // counter for each bit in the color code
+uint8_t frame_buffer[NUM_OF_PIX*BYTES_PER_PIX];
+uint16_t current_byte = 0;                 // counter for each led
+uint16_t current_bit = 0;                  // counter for each bit in the color code
 
 
 // display_init
@@ -43,11 +43,6 @@ void display_init()
     T2CONbits.TCKPS0 = 0;                           // set to 1:1 operation
     TMR2 = 0;                                       // reset the TMR2 counting reg
     PR2 = BIT_SEND_CYCLES - 1;                      // 1.5us cycle
-    
-    // setup the timer2 interrupt
-    _T2IF = 0;
-    _T2IE = 1;
-    _T2IP = 3; // TODO what priority
     
     // set the background to all off
     set_background(0, 0, 0);
@@ -148,7 +143,7 @@ DISPLAY_ERR set_pixel(uint8_t xpos, uint8_t ypos,
 void update_display(void)
 {
     // reset the counter variables
-    current_bit = 0b80;
+    current_bit = 0x80;
     current_byte = 0;
     
     // get the first bit in the sequence and set OC1RS as needed
@@ -166,26 +161,70 @@ void update_display(void)
     // reset to right after the falling edge so OC1 starts low
     TMR2 = WRITE_0_HIGH_CYCLES+1;
     
-    // start timer2
+    // this will hang while outputting the bit sequence, and will determine if the
+    // next bit sent will need to be a 0 or a 1. It is written in assembly to be
+    // extra speedy
+    
+    // push all of our working regs to the stack
+    asm("push w0");                     // used as a temp reg for movement
+    asm("push w1");
+    asm("push w2");
+    asm("push w3");
+    asm("push w4");
+    
+    // put T2IF(IFS0 bit 7), OC1RS, current_byte and bit in working regs, we will be using them a lot
+    WREG1 = IFS0;
+    WREG2 = OC1RS;
+    WREG3 = current_byte;
+    WREG4 = current_bit;
+        
+    // start timer2 beginning the whole process
     T2CONbits.TON = 1;
     
-    // this will hang while timer2 ISR is running. The ISR will disable itself when
-    // the time is correct, ending the loop
-    while (T2CONbits.TON);
-}
-
-
-// timer2 ISR
-//  The timer2 interrupt will need to be as fast as possible, which is why it is
-//  written in assembly. This function will figure out what the next bit in the
-//  sequence needs to be and sets OC1RS accordingly
-void __attribute__((__interrupt__, __auto_psv__)) _T2Interrupt(void)
-{
-    // reset the interrupt flag
-    _T2IF = 0;
+    asm("START:");
     
+    // wait here until timer2 resets (TODO this may not work, I think OC1 resets this)
+    while(!_T1IF);
     
+    // right shift the current_bit by 1
+    asm("mov #0x01, w0");
+    asm("asr [w4], w0");
     
+    // check if the current_bit underflowed, meaning we need to move on to the next byte
+    asm("bra NC, CHECK_END");
+    
+    // the current_bit underflowed, increase the current_byte and set current_bit to 0x80
+    asm("inc [w3], [w3]");
+    asm("mov #0x80, w0");
+    asm("mov w0, [w4]");
+    
+    asm("CHECK_END:");
+    // check if the current_byte is at the end of the array, meaning we are done
+    // sending bits and should jump to the end and turn off the timer2
+    if (current_byte >= TOTAL_BYTES)
+    {
+        asm("bra END");
+    }
+    
+    // check if the next bit in the sequence is a 1 or a 0 and set OC1RS accordingly
+    if (frame_buffer[current_byte] & current_bit)
+        OC1RS = WRITE_1_HIGH_CYCLES;
+    
+    else
+        OC1RS = WRITE_0_HIGH_CYCLES;
+    
+    asm("bra START");
+    
+    asm("END:");
+    while(!_T1IF);                  // wait until the last cycle is over (TODO this may not work as OC1 resets the bit)
+    T2CONbits.TON = 0;              // turn off the timer, this will also disable OC1
+    
+    // pop everything off the stack
+    asm("pop w4");
+    asm("pop w3");
+    asm("pop w2");
+    asm("pop w1");
+    asm("pop w0");
 }
 
 
